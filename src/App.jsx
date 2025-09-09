a9d4b9a1ff3f969cecc467636ee4c609a836fc71
Patrick <jtpanizales@up.edu.ph>
2025-09-09 12:24:59 +0800

    feat(ui): extract Form to its own component and added Post component to handle display of posts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import { useState } from 'react'
 import axios from "axios";
 
-function App() {
+function Posts({posts}) {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title.rendered}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-const [url, setUrl] = useState("")
-const [categoryId, setCategoryId] = useState(1)
-const [date, setDate] = useState("")
-const [posts, setPosts] = useState([])
+function Form(props){
+  return(
+    <>
+      <form onSubmit={props.onSubmit}>
+        <div>
+          <label htmlFor="url">WordPress Site URL: </label>
+          <input type="url" id= "url" name="url" placeholder="Enter URL of WordPress site here" value={props.url} required onChange={e => props.setUrl(e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="category-id">Category id: </label>
+          <input type="number" id= "category-id" name="category-id" min="1" step="1" value={props.categoryId} onChange={e => props.setCategoryId(e.target.value)}/>
+        </div>
+
+        <div>
+          <label htmlFor="date">Date published: </label>
+          <input type="date" id= "date" name="date"  value={props.date} onChange={e => props.setDate(e.target.value)}/>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+}
+
+function App() {
+  const [url, setUrl] = useState("")
+  const [categoryId, setCategoryId] = useState(1)
+  const [date, setDate] = useState("")
+  const [posts, setPosts] = useState([])
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -27,26 +64,14 @@ const [posts, setPosts] = useState([])
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="url">WordPress Site URL: </label>
-          <input type="url" id= "url" name="url" placeholder="Enter URL of WordPress site here" value={url} required onChange={e => setUrl(e.target.value)} />
-        </div>
-
-        <div>
-          <label htmlFor="category-id">Category id: </label>
-          <input type="number" id= "category-id" name="category-id" min="1" step="1" value={categoryId} onChange={e => setCategoryId(e.target.value)}/>
-        </div>
-
-        <div>
-          <label htmlFor="date">Date published: </label>
-          <input type="date" id= "date" name="date"  value={date} onChange={e => setDate(e.target.value)}/>
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
+      <Form onSubmit={handleSubmit} 
+            setUrl={setUrl}
+            setCategoryId={setCategoryId}
+            setDate={setDate}
+      />
+      <Posts posts={posts}/>
     </>
-  )
+    )
 }
 
 export default App
