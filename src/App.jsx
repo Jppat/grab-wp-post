99de3364065ff35ab5cbd76a9f3c39c871be3b79
Patrick <jtpanizales@up.edu.ph>
2025-09-11 14:05:44 +0800

    refactor: each post is now rendered in its own component
    before, post, although, its own component, still
    processed the whole collection of post. Now, only
    a single post is processed by the post component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,49 @@ import TurndownService from 'turndown';
 import Markdown from 'react-markdown'
 import './App.css';
 
-function Post({posts}) {
+function Post({post}) {
+
+    const [isTextCopied, setIsTextCopied] = useState(false);
 
     async function copyButton(title, content){
-    const contentByParagraph = content.split(/\n+/)
-    const copiedText = `${title}\n${contentByParagraph[0]}`
-    try {
-        await navigator.clipboard.writeText(copiedText);
-        console.log('Copied to clipboard:', copiedText);
-      } catch (err) {
-        console.error('Failed to copy:', err);
-      }
+      setIsTextCopied(true);
+      const contentByParagraph = content.split(/\n+/);
+      const copiedText = `${title}\n${contentByParagraph[0]}`;
+      try {
+          await navigator.clipboard.writeText(copiedText);
+          console.log('Copied to clipboard:', copiedText);
+        } catch (err) {
+          console.error('Failed to copy:', err);
+        }
   }
   return(
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>
-          <Markdown>{post.title}</Markdown>
-          <Markdown>{post.excerpt}</Markdown>
-          <button onClick={() => copyButton(post.title, post.content)}>Copy</button>
-        </li>
-      ))}
-    </ul>
+    <li key={post.id} className={isTextCopied && "copiedText"} >
+      <Markdown>{post.title}</Markdown>
+      <Markdown>{post.excerpt}</Markdown>
+      <button onClick={() => copyButton(post.title, post.content)}>Copy</button>
+      {console.log(isTextCopied)}
+    </li>
   )
 }
 
 function Posts({posts}) {
-  const turndownService = new TurndownService()
+  const turndownService = new TurndownService();
   let post_info = posts.map(post => {
     let id = post.id;
-    let title = turndownService.turndown(`<h3>${DOMPurify.sanitize(post.title.rendered)}</h3>`)
-    let excerpt = turndownService.turndown(DOMPurify.sanitize(post.excerpt.rendered))
-    let content = turndownService.turndown(DOMPurify.sanitize(post.content.rendered))
-    return ({id, title, excerpt, content})
+    let title = turndownService.turndown(`<h3>${DOMPurify.sanitize(post.title.rendered)}</h3>`);
+    let excerpt = turndownService.turndown(DOMPurify.sanitize(post.excerpt.rendered));
+    let content = turndownService.turndown(DOMPurify.sanitize(post.content.rendered));
+    return ({id, title, excerpt, content});
   })
   const final_post = post_info.filter(post => post.excerpt != "");
   return (
     <>
       {(final_post.length != 0) && <h2>Posts</h2>}
-      <Post posts={final_post} />
+      <ul>
+        {final_post.map(post => (
+          <Post post={post} />
+        ))}
+      </ul>
     </>
   );
 }
@@ -80,19 +84,19 @@ function App() {
   const [posts, setPosts] = useState([])
 
   async function handleSubmit(e) {
-    e.preventDefault()
-    let params = []
+    e.preventDefault();
+    let params = [];
 
-    categoryId && params.push(`categories=${categoryId}`)
-    date && params.push(`after=${date}T00:00:00&before=${date}T23:59:59`)
+    categoryId && params.push(`categories=${categoryId}`);
+    date && params.push(`after=${date}T00:00:00&before=${date}T23:59:59`);
 
-    const request = `${url}/posts?${params && params.join('&')}`
+    const request = `${url}/posts?${params && params.join('&')}`;
     try {
       const response = await axios.get(request);
       setPosts(response.data); // return just the data from the API
     } catch (error) {
       console.error("Error fetching user data:", error.message);
-      setPosts([])
+      setPosts([]);
     }
   }
 
