d4fc40b68d257e27b129a3c976ff30571f98bd6a
Patrick <jtpanizales@up.edu.ph>
2025-11-28 01:08:32 +0800

    undo some config stuff

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import fs from 'fs'
+import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/grab-wp-post',
