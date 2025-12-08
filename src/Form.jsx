c2cf788de4578b34b1ce2579f3fea57c3cf93ebd
Patrick <jtpanizales@up.edu.ph>
2025-12-08 12:17:33 +0800

    remove code smells

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,38 +1,9 @@
-import { useState, useEffect } from 'react';
 import Button from './Button';
-import createAxiosInstance from './axiosInstance';
-
-function useDebounce(value, delay = 500) {
-  const [debouncedData, setDebouncedData] = useState([]);
-  console.log(`useDebounce called with value: ${value}, debouncedData: ${debouncedData}`);
-
-  useEffect(() => {
-    if (!value) return;
-
-    const handler = setTimeout(async () => {
-      try {
-        const axiosInstance = createAxiosInstance();
-        const response = await axiosInstance.get(
-          `https://www.watchmendailyjournal.com/wp-json/wp/v2/categories?slug=${value}&_fields=id,name,slug`
-        );
-        setDebouncedData(response);
-        console.log("Fetched category data:", response);
-      } catch (error) {
-        console.error("Error fetching category data:", error);
-      }
-    }, delay);
-
-    return () => clearTimeout(handler);
-  }, [value, delay]);
-
-  return debouncedData;
-}
+import useDebounce from './debounce';
 
 function Form(props){
-  console.log(`form rendered, category: ${props.category}`);
   const fetchedCategory = useDebounce(props.category, 2000);
-  console.log("debounced categories:", fetchedCategory);
-  
+
   return(
     <>
       <form
